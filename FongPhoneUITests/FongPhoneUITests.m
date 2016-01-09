0a91f27d9e0afa72719b22e738076b2ca9e48a93
trouscaillon <andrew.r.allred@gmail.com>
2016-01-09 15:22:07 -0500

    UI tests

---
+++ FongPhoneUITests/FongPhoneUITests.m
@@ -0,0 +1,47 @@
+//
+//  FongPhoneUITests.m
+//  FongPhoneUITests
+//
+//  Created by Andrew Allred on 1/9/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FongPhoneUITests : XCTestCase
+
+@end
+
+@implementation FongPhoneUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    
+    XCUIElementQuery *tabBarsQuery = [[XCUIApplication alloc] init].tabBars;
+    [tabBarsQuery.buttons[@"Sound"] tap];
+    [tabBarsQuery.buttons[@"Notes/Loops"] tap];
+    [tabBarsQuery.buttons[@"States"] tap];
+    [tabBarsQuery.buttons[@"Fongs"] tap];
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
